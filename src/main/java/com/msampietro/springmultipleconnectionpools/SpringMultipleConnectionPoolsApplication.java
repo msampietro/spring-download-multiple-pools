ed9aba4262733034a222e6fef971fae2cdfc725f
msampietro <msampietro93@gmail.com>
2020-12-19 20:06:45 -0300

    repo init

---
+++ src/main/java/com/msampietro/springmultipleconnectionpools/SpringMultipleConnectionPoolsApplication.java
@@ -0,0 +1,13 @@
+package com.msampietro.springmultipleconnectionpools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMultipleConnectionPoolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMultipleConnectionPoolsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/msampietro/springmultipleconnectionpools/SpringMultipleConnectionPoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msampietro.springmultipleconnectionpools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMultipleConnectionPoolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
